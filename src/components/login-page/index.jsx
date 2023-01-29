e7b24f3d35cada67005d7a9a315adcb6375a609b
RafaelSabin0 <rafaelarrudaolimpio@gmail.com>
2023-01-29 16:41:20 -0300

    Creating login page

---
+++ src/components/login-page/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css'
+import intronLogo from '../../svg/intronLogo2.svg'
+import { Button, Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material';
+import computerLady from '../../images/computer-lady.png';
+
+export default function LoginPage(){
+    return(
+        <div className="login-box">
+            <img className='intron-logo' src={intronLogo} alt="intron logo" />
+        
+        <div className="login-form">
+            <h1>Candidate Login</h1>
+            <div className='username-input'>
+            <TextField id="outlined-basic" label="Username" variant="outlined"/>
+            </div>
+            <div className="password-input">
+            <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password"/>
+            </div>
+            <div className="confirmation-checkbox">
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox defaultChecked="false"/>} label="Keep me looged in"/>
+                </FormGroup>
+            </div>
+            <Button className="login-button" variant="contained">Login</Button>
+        </div>
+        <div className="loginCardImg">
+            <img src={computerLady} alt="lady looking to a computer screen"/>
+        </div>
+        </div>
+    )
+}
